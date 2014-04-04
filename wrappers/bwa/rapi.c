847791aa57ee51fb72babb242d3054b26e45f3a4
Luca Pireddu <pireddu@crs4.it>
2014-04-04 20:15:02 +0200

    Compiles and loads the reference (hard-coded path)
    On the other hand, aln_set_read returns an error.

--- wrappers/bwa/rapi.c
+++ wrappers/bwa/rapi.c
@@ -1,7 +1,8 @@
 
 
-#include <aligner.h>
+#include "aligner.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 
 /* SAM flags */
@@ -16,30 +17,35 @@
 #define SAM_FSC 256 // secondary alignment
 
 
-const char * read_pair[] = {
-	"read id",
-	"TAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCAACCCCAACCCCAACCC",
-	"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-	"TAACCCTAACCCCTAACCCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAAC",
-	"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+const char * read_pair[][5] = {
+	{
+		"read id",
+		"TAACCCTAACCCTAACCCTAACCCTAACCCCAACCCCAACCCCAACCCCAACCCCAACCC",
+		"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+		"TAACCCTAACCCCTAACCCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAAC",
+		"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+	}
 };
 
-void check_error(int code, const char* error_msg) {
-	if (code) {
-		fprintf(stderr, error_msg);
+void check_error(int code, const char* error_msg)
+{
+	if (code)
+	{
+		fprintf(stderr, "%s\n", error_msg);
 		fprintf(stderr, "\nError code: %d\n", code);
 		abort();
 	}
 }
 
-void print_alignments(const aln_batch* batch, FILE* out) {
+void print_alignments(const aln_batch* batch, FILE* out)
+{
 	for (int f = 0; f < batch->n_frags; ++f) {
-		for (int i = 0; i < batch->n_reads_frags; ++i) {
-			const aln_read* read = aln_get_read(batch, f, i);
+		for (int i = 0; i < batch->n_reads_frag; ++i) {
+			const aln_read*const read = aln_get_read(batch, f, i);
 
-			fprintf(out, read->id);
+			fprintf(out, "%s", read->id);
 
-			const aln_alignment* aln = &read->alignment;
+			const aln_alignment*const aln = &read->alignment;
 			int flag = 0;
 			flag |= aln->paired         ? SAM_FPD : 0;
 			flag |= aln->prop_paired    ? SAM_FPP : 0;
@@ -58,58 +64,59 @@ void print_alignments(const aln_batch* batch, FILE* out) {
 				fprintf(out, "\t%.*s", read->length, read->qual);
 			else
 				fprintf(out, "\t*");
-
+			// more stuff... insert size and tags
 		}
 	}
 }
 
 
-int main(int arc, const char* argv[]) {
+int main(int argc, const char* argv[])
+{
 	int error = 0;
-
-	error = aln_init();
-	check_error(error, "Failed to initialize");
-
-	fprintf(stderr, "Initialized library version %s", aln_version());
 
 	aln_opts opts;
 	error = aln_init_opts(&opts);
 	check_error(error, "Failed to init opts");
 
-	const aln_kv* tail = opts.parameters;
-	opts.parameters = aln_kv_insert_long("-t", 2, tail);
+	//const aln_kv* tail = opts.parameters;
+	//opts.parameters = aln_kv_insert_long("-t", 2, tail);
+	//check_error(opts.parameters != NULL, "Failed to insert parameters");
 
-	check_error(opts.parameters != NULL, "Failed to insert parameters");
+	error = aln_init(&opts);
+	check_error(error, "Failed to initialize");
+
+	fprintf(stderr, "Initialized library version '%s'\n", aln_version());
 
 	aln_ref ref;
-	error = aln_load_ref("/home/pireddu/Projects/bwa_wrapper/mini_ref/mini_ref", &ref);
+	error = aln_load_ref("/u/pireddu/Projects/bwa_wrapper/mini_ref/mini_ref.fasta", &ref);
 	check_error(error, "Failed to load reference");
 
+	fprintf(stderr, "This reference has %d contigs\n", ref.n_contigs);
 
 	aln_batch reads;
 	error = aln_alloc_reads(&reads, 2, 1);//sizeof(read_pair) / (3 * sizeof(char*)));
 	check_error(error, "Failed to allocate read batch");
 
-	for (int f = 0; f < reads->n_frags; ++f) {
+	for (int f = 0; f < reads.n_frags; ++f) {
 		const char* read_id = read_pair[f][0];
-		for (int r = 1; r <= reads->n_reads_frags; ++r) {
-			error = aln_set_read(f, r, read_id, read_pair[f][r], read_pair[f][r+1]);
+		for (int r = 1; r <= reads.n_reads_frag; ++r) {
+			error = aln_set_read(&reads, f, r, read_id, read_pair[f][r], read_pair[f][r+1], ALN_QUALITY_ENCODING_SANGER);
 			check_error(error, "Failed to set read");
 		}
 	}
 
 	fprintf(stderr, "Allocated read batch\n");
-	fprintf(stderr, "\tn_allocated_frags: %d; n_frags: %d; n_reads_frags: %d\n",
-			reads.n_allocated_frags, reads.n_frags, reads.n_reads_frags);
+	fprintf(stderr, "\tn_frags: %d; n_reads_frag: %d\n",
+			reads.n_frags, reads.n_reads_frag);
 
-	error = aln_align_reads(&reads, &opts);
-	check_error(error, "Failed to align reads!");
+	//error = aln_align_reads(&reads, &opts);
+	//check_error(error, "Failed to align reads!");
 
-	fprintf(stderr, "Reads aligned\n");
+	//fprintf(stderr, "Reads aligned\n");
 
-	check_error(error, "Failed to align reads!");
 	aln_free_ref(&ref);
 	aln_free_reads(&reads);
+	aln_free_opts(&opts);
 
 	return 0;
 }
--- wrappers/bwa/rapi_bwa.c
+++ wrappers/bwa/rapi_bwa.c
@@ -5,8 +5,12 @@
 #include "aligner.h"
 #include "bwamem.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+char* bwa_pg = "rapi";
 
+#if 0
 aln_kv* aln_kv_insert_char(const char* key, char value,           const aln_kv* tail);
 aln_kv* aln_kv_insert_str( const char* key, const char* value,    const aln_kv* tail);
 aln_kv* aln_kv_insert_long(const char* key, long value,           const aln_kv* tail);
@@ -15,9 +19,9 @@ aln_kv* aln_kv_insert_ba(  const char* key, const uint8_t* value, const aln_kv* 
 aln_kv* aln_kv_insert_la(  const char* key, const long* value,    const aln_kv* tail);
 
 aln_kv_free_list(aln_kv* list);
+#endif
 
-
-#if 0
+#if 1 // strdup is not defined if we compile with c99
 char* strdup(const char* str)
 {
 	if (NULL == str)
@@ -32,23 +36,32 @@ char* strdup(const char* str)
 #endif
 
 /* Init Library */
-int aln_init()
+int aln_init(const aln_opts* opts)
 {
-	// no op
+	mem_opt_t*const bwa_opt = (mem_opt_t*) opts->_private;
+
+	bwa_fill_scmat(bwa_opt->a, bwa_opt->b, bwa_opt->mat);
+
 	return ALN_NO_ERROR;
 }
 
 /* Init Library Options */
-int aln_init_ops( aln_opts * my_opts )
+int aln_init_opts( aln_opts * my_opts )
 {
-	// create a BWA opt structure and link it to the external one
-	my_opts->_private = mem_opt_init();
+	// create a BWA opt structure
+	mem_opt_t*const bwa_opt = mem_opt_init();
 
-	my_opts->ignore_unsupported = TRUE;
+	// set the default values as in BWA fastmap.c
+	bwa_opt->a = bwa_opt->b = bwa_opt->o_del = bwa_opt->e_del = -1;
+	bwa_opt->o_ins = bwa_opt->e_ins = bwa_opt->pen_unpaired = -1;
+	bwa_opt->pen_clip5 = bwa_opt->pen_clip3 = bwa_opt->zdrop = bwa_opt->T = -1;
+
+	my_opts->_private = bwa_opt; // hand the bwa structure onto the external one
+	my_opts->ignore_unsupported = 1;
 	my_opts->mapq_min     = 0;
 	my_opts->trim_quality = 0;
 	my_opts->isize_min    = 0;
-	my_opts->isize_max    = my_opts->_private.max_ins;
+	my_opts->isize_max    = bwa_opt->max_ins;
 	my_opts->n_parameters = 0;
 	my_opts->parameters   = NULL;
 
@@ -73,66 +86,77 @@ int aln_load_ref( const char * reference_path, aln_ref * ref_struct )
 	if ( NULL == ref_struct || NULL == reference_path )
 		return ALN_PARAM_ERROR;
 
-	ref_struct->_private = bwa_idx_load(reference, BWA_IDX_ALL);
-
-	if ( NULL == ref_struct->_private )
+	const bwaidx_t*const bwa_idx = bwa_idx_load(reference_path, BWA_IDX_ALL);
+	if ( NULL == bwa_idx )
 		return ALN_REFERENCE_ERROR;
- 
+
+	// allocate memory
 	ref_struct->path = strdup(reference_path);
- 
-	/* Fill in Contig Information */
-	ref_struct->n_contigs = reference->handle->bns->n_seqs; /* Handle contains bnt_seq_t * bns holding contig information */
-	ref_struct->contigs = calloc( ref_struct->n_contigs, sizeof(aln_contig) );
- 
-	if ( NULL == ref_struct->contigs )
+	ref_struct->contigs = calloc( bwa_idx->bns->n_seqs, sizeof(aln_contig) );
+	if ( NULL == ref_struct->path || NULL == ref_struct->contigs )
 	{
+		// if either allocations we free everything and return an error
+		bwa_idx_destroy((bwaidx_t*)bwa_idx);
+		free(ref_struct->path);
+		free(ref_struct->contigs);
+		memset(ref_struct, 0, sizeof(*ref_struct));
 		return ALN_MEMORY_ERROR;
 	}
  
-	for ( int contig_loop = 0, contig_loop < ref_struct->n_contigs; contig_loop++ )
+	/* Fill in Contig Information */
+	ref_struct->n_contigs = bwa_idx->bns->n_seqs; /* Handle contains bnt_seq_t * bns holding contig information */
+	for ( int i = 0; i < ref_struct->n_contigs; ++i )
 	{
-		/* Get Name */
-		/* Each bns holds an array of bntann1_t *anns -> holding
-		     int64_t offset;
-		     int32_t len;
-		     int32_t n_ambs;
-		     uint32_t gi;
-		     char *name, *anno;
-		*/
-		ref_struct->contigs[ contig_loop ].name = reference->handle->bns->anns[ contig_loop ].name;
-		ref_struct->contigs[ contig_loop ].len = reference->handle->bns->anns[ contig_loop ].len;
+		ref_struct->contigs[ i ].name = bwa_idx->bns->anns[ i ].name;
+		ref_struct->contigs[ i ].len = bwa_idx->bns->anns[ i ].len;
 	}
+	ref_struct->_private = (bwaidx_t*)bwa_idx; // cast to discard const
 
 	return ALN_NO_ERROR;
 }
 
 /* Free Reference */
-int aln_free_ref( aln_ref * ref_struct )
+int aln_free_ref( aln_ref * ref )
 {
-	free(ref_struct->path);
+	// free bwa's part
+	bwa_idx_destroy(ref->_private);
 
-	for ( int contig_loop = 0, contig_loop < ref_struct->n_contigs; contig_loop++ )
-	{
-		free (	ref_struct->contigs );
+	// then free the rest of the structure
+	free(ref->path);
+
+	if (ref->contigs) {
+		for ( int i = 0; i < ref->n_contigs; ++i )
+		{
+			aln_contig* c = &ref->contigs[i];
+			free(c->name);
+			free(c->assembly_identifier);
+			free(c->species);
+			free(c->uri);
+		}
+		free (ref->contigs);
 	}
+	memset(ref, 0, sizeof(*ref));
+	return ALN_NO_ERROR;
 }
 
 /* Allocate reads */
-int aln_alloc_reads( aln_batch * batch, int n_reads_fragment, int n_fragments );
+int aln_alloc_reads( aln_batch * batch, int n_reads_fragment, int n_fragments )
 {
 	if (n_fragments < 0 || n_reads_fragment < 0)
 		return ALN_PARAM_ERROR;
 
-	batch = calloc( n_reads_fragment * n_fragments, sizeof(aln_batch) );
-	batch->n_fragments = n_fragments;
-	batch->n_reads_fragment = n_reads_fragment;
-	return ( batch == 0 ? ALN_MEMORY_ERROR : ALN_NO_ERROR );
+	batch->reads = calloc( n_reads_fragment * n_fragments, sizeof(aln_read) );
+	if (NULL == batch->reads)
+		return ALN_MEMORY_ERROR;
+	batch->n_frags = n_fragments;
+	batch->n_reads_frag = n_reads_fragment;
+	return ALN_NO_ERROR;
 }
 
 int aln_free_reads( aln_batch * batch )
 {
 	for (int f = 0; f < batch->n_frags; ++f) {
-		for (int r = 0; r < batch->n_reads_frags; ++r) {
+		for (int r = 0; r < batch->n_reads_frag; ++r) {
 			aln_read* read = aln_get_read(batch, f, r);
 			free(read->id);
 			free(read->seq);
@@ -146,7 +170,7 @@ int aln_free_reads( aln_batch * batch )
 	return ALN_NO_ERROR;
 }
 
-int aln_set_read(const aln_batch* batch,
+int aln_set_read(aln_batch* batch,
 			int n_frag, int n_read,
 			const char* name, const char* seq, const char* qual,
 			int q_offset) {
@@ -155,19 +179,19 @@ int aln_set_read(const aln_batch* batch,
 	if (n_frag >= batch->n_frags || n_read >= batch->n_reads_frag)
 		return ALN_PARAM_ERROR;
 
-	aln_read* seq = aln_get_read(batch, n_frag, n_read);
+	aln_read* read = aln_get_read(batch, n_frag, n_read);
 	const int name_len = strlen(name);
 	const int seq_len = strlen(seq);
-	seq->length = seq->clip_len = seq_len;
+	read->length = read->clip_len = seq_len;
 
-	seq->seq = (char*)malloc(seq_len);
-	seq->name = (char*)malloc(name_len + 1); // +1 for \0
+	read->seq = (char*)malloc(seq_len);
+	read->id = (char*)malloc(name_len + 1); // +1 for \0
 	if (qual)
-		seq->qual = (ubyte_t*)malloc(seq_len);
+		read->qual = (ubyte_t*)malloc(seq_len);
 	else
-		seq->qual = NULL;
+		read->qual = NULL;
 
-	if (seq->seq == NULL || seq->name == NULL || (seq->qual == NULL && qual != NULL))
+	if (read->seq == NULL || read->id == NULL || (read->qual == NULL && qual != NULL))
 	{
 		fprintf(stderr, "Unable to allocate memory for sequence\n");
 		error_code = ALN_MEMORY_ERROR;
@@ -177,16 +201,16 @@ int aln_set_read(const aln_batch* batch,
 
 	// encode sequence and set quality
 	/** XXX: should we recode the bases here? */
-	memcpy(seq->seq, seq, seq_len);
+	memcpy(read->seq, seq, seq_len);
 
-	if (seq->qual)
+	if (read->qual)
 	{
 		for (int i = 0; i < seq_len; ++i)
 		{
-			seq->qual[i] = (int)qual[i] - q_offset + 33; // 33 is the Sanger offset.  BWA expects it this way.
-			if (seq->qual[i] > 127)
+			read->qual[i] = (int)qual[i] - q_offset + 33; // 33 is the Sanger offset.  BWA expects it this way.
+			if (read->qual[i] > 127)
 			{ // qual is unsigned, by Sanger base qualities have an allowed range of [0,94], and 94+33=127
-				fprintf(stderr, "Invalid base quality score %d\n", seq->qual[i]);
+				fprintf(stderr, "Invalid base quality score %d\n", read->qual[i]);
 				error_code = ALN_PARAM_ERROR;
 				goto error;
 			}
@@ -194,24 +218,34 @@ int aln_set_read(const aln_batch* batch,
 	}
 
 	// and finally the name
-	strcpy(seq->id, name);
+	strcpy(read->id, name);
 	// trim /[12]$
 	int t = name_len;
-	if (t > 2 && seq->id[t-2] == '/' && (seq->id[t-1] == '1' || seq->id[t-1] == '2'))
-		seq->name[t-2] = '\0';
+	if (t > 2 && read->id[t-2] == '/' && (read->id[t-1] == '1' || read->id[t-1] == '2'))
+		read->id[t-2] = '\0';
 	return 0;
 
 error:
 	// In case of error, free any allocated memory and return -1
-	free(seq->seq); free(seq->id); free(seq->qual);
+	free(read->seq); free(read->id); free(read->qual);
 	return error_code;
 }
 
-int aln_align_reads( aln_batch * batch, const aln_opts * config )
+int aln_align_reads( const aln_ref* ref,  aln_batch * batch, const aln_opts * config )
 {
+	/*
+	mem_opt_t*const bwa_opt = (mem_opt_t*) config->_private;
+	
+	const bwaidx_t*const bwa_idx = ref->_private;
+
+	mem_alnreg_v bwa_alignment;
+	*/
+
 	// traslate our read structure into BWA reads
 
 	// run the alignment
+	//bwa_alignment = mem_align1(bwa_opt, bwa_idx->bwt, bwa_idx->bns, bwa_idx->pac, ks->seq.l, ks->seq.s); // get all the hits
 	// translate BWA's results back into our structure.
+	return ALN_NO_ERROR;
 }
 
