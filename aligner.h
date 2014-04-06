7854ae0cbbada8dd8c334900c886f751ae8ea19a
Luca Pireddu <pireddu@crs4.it>
2014-04-06 13:29:23 +0200

    Now compiles

--- aligner.h
+++ aligner.h
@@ -1,66 +1,44 @@
 /*
-  aligner.h
+ * RAPI - the Read aligner API
+ *
+ * Authors:  Luca Pireddu, Riccardo Berutti, Ridvan Dongelci
+ */
 
-  Authors:  Riccardo Berutti, Luca Pireddu, Ridvan Dongelci
-*/
+#ifndef __RAPI_H__
+#define __RAPI_H__
 
-#define ALN_TAG_TYPE_CHAR		1
-#define ALN_TAG_TYPE_TEXT		2
-#define ALN_TAG_TYPE_INT		3
-#define ALN_TAG_TYPE_REAL		4
-#define ALN_TAG_TYPE_BYTE_ARRAY		5
-#define ALN_TAG_TYPE_INT_ARRAY		6
+#include <stdint.h>
 
-#define ALN_QUALITY_ENCODING_SANGER	33
-#define ALN_QUALITY_ENCODING_ILLUMINA	64
+/* Error types */
+#define ALN_NO_ERROR                     0
+#define ALN_GENERIC_ERROR               -1
+#define ALN_REFERENCE_ERROR            -10
 
-/* Structs */
+#define ALN_ALIGNMENT_OK                 0
+#define ALN_ALIGNMENT_TAG_NOT_EXISTING -20
 
-typedef struct {
-	 char * name;
-	 void * value;
-} aln_parameter;
+#define ALN_MEMORY_ERROR               -30
+#define ALN_PARAM_ERROR                -40
 
-typedef struct {
-	int ignore_unsupported;
-	/* Standard Ones - Differently implemented by aligners*/
-	uint8_t mapq_min;
-	uint8_t trim_quality;
-	uint8_t isize_min;
-	uint16_t isize_max;
-	uint8_t seed_len;
-	/* Mismatch / Gap_Opens / Quality Trims --> Generalize ? */
-	/* Aligner specific parameters */
-	int n_parameters;
-	aln_parameter * parameters;
-} aln_config;
+/* Key-value TYPES */
 
+#define ALN_VTYPE_CHAR       1
+#define ALN_VTYPE_TEXT       2
+#define ALN_VTYPE_INT        3
+#define ALN_VTYPE_REAL       4
+#define ALN_VTYPE_BYTE_ARRAY 5
+#define ALN_VTYPE_INT_ARRAY  6
 
-typedef struct {
-	const char * name;
-	uint32_t length;
-	const char * assembly_identifier;
-	const char md5[32];
-	const char * species;
-	const char * uri;
-} aln_contig;
+/* FASTQ Quality Encoding */
 
-typedef struct {
-	const char * path;
-	int n_contigs;
-	aln_contig * contigs;
-	void * my_reference;
-} aln_ref;
-
-typedef struct {
-	uint32_t op:4,
-		 len:28;
-} aln_cigar;
+#define ALN_QUALITY_ENCODING_SANGER 33
+#define ALN_QUALITY_ENCODING_ILLUMINA 64
 
 
-typedef struct {
-	char * name;
-	unsigned char type;
+/* Key-value list */
+typedef struct aln_kv {
+	const char * key;
+	uint8_t type;
 	union {
 		char character;
 		const char * text;
@@ -68,73 +46,155 @@ typedef struct {
 		double real;
 		const uint8_t * byte_array;
 		const long * integer_array;
-	} data;
-} aln_tag;
+	} value;
 
-  
+	const struct aln_kv * next;
+} aln_kv;
+
+aln_kv* aln_kv_insert_char(const char* key, char value,           const aln_kv* tail);
+aln_kv* aln_kv_insert_str( const char* key, const char* value,    const aln_kv* tail);
+aln_kv* aln_kv_insert_long(const char* key, long value,           const aln_kv* tail);
+aln_kv* aln_kv_insert_dbl( const char* key, double value,         const aln_kv* tail);
+aln_kv* aln_kv_insert_ba(  const char* key, const uint8_t* value, const aln_kv* tail);
+aln_kv* aln_kv_insert_la(  const char* key, const long* value,    const aln_kv* tail);
+
+int aln_kv_get_char(const aln_kv* kv_list, const char* key, char * value         );
+int aln_kv_get_str( const aln_kv* kv_list, const char* key, const char** value   );
+int aln_kv_get_long(const aln_kv* kv_list, const char* key, long * value         );
+int aln_kv_get_dbl( const aln_kv* kv_list, const char* key, double * value       );
+int aln_kv_get_ba(  const aln_kv* kv_list, const char* key, const uint8_t** value);
+int aln_kv_get_la(  const aln_kv* kv_list, const char* key, const long** value   );
+
+
+// free the key and values??
+int aln_kv_free_list(aln_kv* list);
+
+
+/**
+ * Options.
+ */
+typedef struct {
+	int ignore_unsupported;
+	/* Standard Ones - Differently implemented by aligners*/
+	int mapq_min;
+	int trim_quality;
+	int isize_min;
+	int isize_max;
+	/* Mismatch / Gap_Opens / Quality Trims --> Generalize ? */
+	/* Aligner specific parameters */
+	int n_parameters;
+	aln_kv * parameters;
+
+	void * _private; /**< can be used for aligner-specific data */
+} aln_opts;
+
+
+/**
+ * Reference
+ */
+typedef struct {
+	char * name;
+	uint32_t len;
+	char * assembly_identifier;
+	char md5[32];
+	char * species;
+	char * uri;
+} aln_contig;
+	
+typedef struct {
+	char * path;
+	int n_contigs;
+	aln_contig * contigs;
+	void * _private;
+} aln_ref;
+
+
+/**
+ * Read and alignment
+ */
+typedef struct {
+	uint32_t op:4,
+	         len:28;
+} aln_cigar;
+
 typedef struct {
 	uint8_t type;
 	uint8_t len;
-	const char * data;
+	char * data;
 } aln_mismatch;
 
+
 typedef struct {
-	const char * contig;
+	char * contig;
 	unsigned long int pos;
-	aln_tag * tags;
-	uint8_t n_tags;
+	aln_kv * tags;
 	uint8_t mapq;
+
+	uint8_t paired:1,
+	        prop_paired:1,
+	        unmapped:1,
+	        reverse_strand:1,
+	        secondary_aln:1;
+
+	uint8_t n_mismatches;
+	uint8_t n_gap_opens;
+	uint8_t n_gap_extensions;
+	uint16_t edit_distance;
+
 	aln_cigar * cigar_ops;
 	uint8_t n_cigar_ops;
-	uint8_t	paired:1,
-	prop_paired:1,
-	unmapped:1,
-	reverse_strand:1,  
-	secondary_aln:1;
-	uint8_t	n_mismatches;
-	uint8_t	n_gap_opens;
-	uint8_t	n_gap_extensions;
-	uint16_t	edit_distance;
-	aln_mismatch * mismatches;
+	aln_mismatch * mm_def;
+	uint8_t n_mm_defs;
 } aln_alignment;
 
 typedef struct {
 	char * id;
-	unsigned char * seq;
-	unsigned char * qual;
+	char * seq;
+	uint8_t * qual;
 	unsigned int length;
-	unsigned int bp_clipping;
+	unsigned int clip_len;
 	aln_alignment alignment;
 } aln_read;
 
 typedef struct {
-	int n_allocated_frags;
 	int n_frags;
 	int n_reads_frag;
 	aln_read * reads;
 } aln_batch;
 
-
-/* Init Library */
-int aln_init();
-
 /* Init Options */
 int aln_init_opts( aln_opts * my_opts );
+
+int aln_free_opts( aln_opts * my_opts );
+
+/* Init Library */
+int aln_init(const aln_opts* opts);
 
 /* Aligner Version */
 const char * aln_version();
 
 /* Load reference */
-int aln_load_ref( const char * reference, aln_ref * ref_struct );
+int aln_load_ref( const char * reference_path, aln_ref * ref_struct );
 
 /* Free reference */
 int aln_free_ref( aln_ref * ref_struct );
 
 /* Allocate reads */
-int aln_alloc_reads( aln_batch ** batch, int n_reads_fragment, int n_fragments );
+int aln_alloc_reads( aln_batch * batch, int n_reads_fragment, int n_fragments );
+
+/* set read data */
+int aln_set_read(aln_batch * batch, int n_frag, int n_read, const char* id, const char* seq, const char* qual, int q_offset);
 
 /* Free reads */
-int aln_free_reads( aln_batch ** batch );
+int aln_free_reads( aln_batch * batch );
 
 /* Align */
-int aln_align_reads( aln_batch * batch, aln_config * config );
+int aln_align_reads( const aln_ref* ref,  aln_batch * batch, const aln_opts * config );
+
+
+
+inline aln_read* aln_get_read(const aln_batch* batch, int n_fragment, int n_read) {
+	return batch->reads + (n_fragment * batch->n_reads_frag + n_read);
+}
+
+#endif
