60f8afdb5a01010ccba18f232a16bb8109905391
Roman Valls Guimera <roman@incf.org>
2014-07-31 14:22:56 +0200

    Fixed clang compilation error and warning. Now compiles on OSX but SWIG segfaults

--- rapi_bwa/rapi_bwa.c
+++ rapi_bwa/rapi_bwa.c
@@ -346,7 +346,7 @@ int rapi_opts_init( rapi_opts * my_opts )
 	bwa_opt->max_chain_gap = 10000;
 	bwa_opt->max_ins = 10000;
 	bwa_opt->mask_level = 0.50;
-	bwa_opt->chain_drop_ratio = 0.50;
+	bwa_opt->XA_drop_ratio = 0.50;
 	bwa_opt->split_factor = 1.5;
 	bwa_opt->chunk_size = 10000000;
 	bwa_opt->n_threads = 1;
@@ -996,7 +996,7 @@ int rapi_align_reads( const rapi_ref* ref,  rapi_batch * batch, const rapi_opts 
 
 	// run the alignment
 	state->n_reads_processed += bwa_seqs.n_reads;
-	fprintf(stderr, "processed %ld reads\n", state->n_reads_processed);
+	fprintf(stderr, "processed %lld reads\n", state->n_reads_processed);
 
 clean_up:
 	free(regs);
