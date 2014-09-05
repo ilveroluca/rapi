623338ca943b5c26a4b180110a4f56fbc7d14d6a
Luca Pireddu <pireddu@crs4.it>
2014-09-05 18:09:57 +0200

    Add notion of loading one of a selection of aligner plugins

--- example/align.py
+++ example/align.py
@@ -4,7 +4,7 @@ import argparse
 import fileinput
 import logging
 
-import pyrapi.rapi as rapi
+import pyrapi
 
 logging.basicConfig(level=logging.INFO)
 _log = logging.getLogger('align')
@@ -19,24 +19,28 @@ def parse_args(args=None):
 
 def main(argv=None):
     options = parse_args(argv)
-    rapi.init(rapi.opts())
+    aligner = pyrapi.load_aligner('rapi_bwa')
+
+    aligner.init(aligner.opts())
+    _log.info("Using the %s aligner, version %s", aligner.aligner_name(), aligner.aligner_version())
+
 
     _log.info("Loading reference %s", options.ref)
-    ref = rapi.ref(options.ref)
+    ref = aligner.ref(options.ref)
     _log.info("Reference loaded")
 
     pe = not options.se
     _log.info("in %s mode", 'paired-end' if pe else 'single-end')
 
-    batch = rapi.read_batch(2 if pe else 1)
+    batch = aligner.read_batch(2 if pe else 1)
 
     _log.info('input: %s', options.input)
     _log.info('reading data')
     for line in fileinput.input(files=options.input):
         row = line.rstrip('\n').split('\t')
-        batch.append(row[0], row[1], row[2], rapi.QENC_SANGER)
+        batch.append(row[0], row[1], row[2], aligner.QENC_SANGER)
         if pe:
-            batch.append(row[0], row[3], row[4], rapi.QENC_SANGER)
+            batch.append(row[0], row[3], row[4], aligner.QENC_SANGER)
 
     _log.info('Loaded %s reads', len(batch))
 
--- pyrapi/__init__.py
+++ pyrapi/__init__.py
@@ -1,2 +1,8 @@
 
-#def load_plugin(name="rapi_bwa"):
+import pyrapi.rapi
+
+def load_aligner(name="rapi_bwa"):
+    """
+    This method is used to load a specific aligner plugin.
+    """
+    return pyrapi.rapi
