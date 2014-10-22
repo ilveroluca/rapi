ebbc4087e12b2853ca8f89b1db9ecdbdd77efda2
Luca Pireddu <pireddu@crs4.it>
2014-10-22 19:14:01 +0200

    Extend unit tests to cover part of the alignment functionality

--- tests/pyrapi/test_pyrapi.py
+++ tests/pyrapi/test_pyrapi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import re
 import sys
 import unittest
 
@@ -222,29 +223,97 @@ class TestPyrapiReadBatch(unittest.TestCase):
         self.assertEquals(1, len(fragment))
 
 
-class TestPyrapiAlign(unittest.TestCase):
+class TestPyrapiAlignment(unittest.TestCase):
     def setUp(self):
         self.opts = rapi.opts()
         rapi.init(self.opts)
         self.ref = rapi.ref(stuff.MiniRef)
+        aligner = rapi.aligner(self.opts)
+        self.batch = rapi.read_batch(2)
+        reads = stuff.get_mini_ref_seqs()
+        read_1 = reads[0]
+        self.batch.append(read_1[0], read_1[1], read_1[2], rapi.QENC_SANGER)
+        self.batch.append(read_1[0], read_1[3], read_1[4], rapi.QENC_SANGER)
+        aligner.align_reads(self.ref, self.batch)
 
     def tearDown(self):
         self.ref.unload()
         rapi.shutdown()
 
-    def test_align_pe(self):
-        aligner = rapi.aligner(self.opts)
-        batch = rapi.read_batch(2)
-        reads = stuff.get_mini_ref_seqs()
-        read_1 = reads[0]
-        batch.append(read_1[0], read_1[1], read_1[2], rapi.QENC_SANGER)
-        batch.append(read_1[0], read_1[3], read_1[4], rapi.QENC_SANGER)
-        aligner.align_reads(self.ref, batch)
+    def test_alignment_struct(self):
+        rapi_read = self.batch.get_read(0, 0)
+        self.assertEqual("read_00", rapi_read.id)
 
-        rapi_read = batch.get_read(0, 0)
         self.assertTrue(rapi_read.n_alignments > 0)
-        rapi_read = batch.get_read(0, 1)
-        self.assertTrue(rapi_read.n_alignments > 0)
+
+        aln = rapi_read.get_aln(0)
+        self.assertEqual("chr1", aln.contig.name)
+        self.assertEqual(32461, aln.pos)
+        self.assertEqual("60M", aln.get_cigar_string())
+        self.assertEqual( ( ('M', 60), ), aln.get_cigar_ops())
+        self.assertEqual(60, aln.mapq)
+        self.assertEqual(60, aln.score)
+        # Flag 65 is 'p1'
+        self.assertTrue(aln.paired)
+        self.assertFalse(aln.prop_paired)
+        self.assertTrue(aln.mapped)
+        self.assertFalse(aln.reverse_strand)
+        self.assertFalse(aln.secondary_aln)
+        self.assertEqual(0, aln.n_mismatches)
+        self.assertEqual(0, aln.n_gap_opens)
+        self.assertEqual(0, aln.n_gap_extensions)
+
+        expected_tags = dict(
+                MD='60',
+                XS=0)
+        # the SAM has more tags (NM and AS), but RAPI makes that information
+        # available through other members of the alignment structure.
+        self.assertEqual(expected_tags, aln.get_tags())
+
+    def test_get_aln_out_of_bounds(self):
+        rapi_read = self.batch.get_read(0, 0)
+        self.assertRaises(IndexError, rapi_read.get_aln, -1)
+        self.assertRaises(IndexError, rapi_read.get_aln, rapi_read.n_alignments)
+
+    def test_alignment_iter(self):
+        return
+        self.fail("Not implemented")
+
+    def test_sam(self):
+        # We ran this command line:
+        #     bwa mem -p -T 0 -a mini_ref/mini_ref.fasta <(head -n 8 mini_ref/mini_ref_seqs.fastq )
+        # First 8 lines == first two reads from fastq file.
+        # BWA version: 0.7.8-r455
+        # The BWA options only serve to tell it not to filter any reads from the output:
+        #    * -p: first query file consists of interleaved paired-end sequences
+        #    * -T 0: minimum score to output [30]
+        #    * -a output all alignments for SE or unpaired PE
+        #
+        # We're using no options with RAPI, which is equivalent to that command line.
+        # This is the output from BWA, without the SAM header.
+        expected = [
+            """read_00	65	chr1	32461	60	60M	=	32581	121	AAAACTGACCCACACAGAAAAACTAATTGTGAGAACCAATATTATACTAAATTCATTTGA	EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE	NM:i:0	MD:Z:60	AS:i:60	XS:i:0""",
+            """read_00	129	chr1	32581	60	60M	=	32461	-121	CAAAAGTTAACCCATATGGAATGCAATGGAGGAAATCAATGACATATCAGATCTAGAAAC	EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE	NM:i:0	MD:Z:60	AS:i:60	XS:i:0"""
+        ]
+
+        rapi_sam = rapi.format_sam(self.batch, 0).split('\n')
+        # The two outputs may not be identical because the order or the tags isn't defined.
+
+        bwa_tag_start = [ re.search(r'\tNM:.*', line).start() for line in expected ]
+        rapi_tag_start = [ re.search(r'\t[A-Z][A-Z]:[A-z]:.*', line).start() for line in rapi_sam ]
+
+        bwa_no_tags = '\n'.join([ s[0:bwa_tag_start[i]] for i, s in enumerate(expected) ])
+        rapi_no_tags = '\n'.join([ s[0:rapi_tag_start[i]] for i, s in enumerate(rapi_sam) ])
+        self.assertEqual(bwa_no_tags, rapi_no_tags)
+
+        # now verify the tags
+        for read_num in xrange(len(bwa_tag_start)):
+            # get the part of the string that contains the tags.  The '+1' is
+            # because the regex we used to find the tags began with a tab, which
+            # we want to skip.
+            bwa_tags = expected[read_num][(bwa_tag_start[read_num]+1):]
+            rapi_tags = rapi_sam[read_num][(rapi_tag_start[read_num]+1):]
+            self.assertEquals(set(bwa_tags.split('\t')), set(rapi_tags.split('\t')))
 
 
 #    def test_align_se(self):
@@ -265,7 +334,7 @@ def suite():
     s = unittest.TestLoader().loadTestsFromTestCase(TestPyrapi)
     s.addTests(unittest.TestLoader().loadTestsFromTestCase(TestPyrapiRef))
     s.addTests(unittest.TestLoader().loadTestsFromTestCase(TestPyrapiReadBatch))
-    s.addTests(unittest.TestLoader().loadTestsFromTestCase(TestPyrapiAlign))
+    s.addTests(unittest.TestLoader().loadTestsFromTestCase(TestPyrapiAlignment))
     return s
 
 def main():
