ad2829fb25358957bb4a4f6dd41b7ee48cd371eb
Luca Pireddu <pireddu@crs4.it>
2014-10-23 16:36:29 +0200

    Typo

--- pyrapi/__init__.py
+++ pyrapi/__init__.py
@@ -3,6 +3,6 @@ import pyrapi.rapi
 
 def load_aligner(name="rapi_bwa"):
     """
-    This method is used to load a specific aligner plugin.
+    This function is used to load a specific aligner plugin.
     """
     return pyrapi.rapi
