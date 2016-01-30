9e4e9f66e1dd92bc4641bf3deb28d21f88858c4d
Luca Pireddu <pireddu@crs4.it>
2016-01-30 01:18:45 +0100

    Avoid multiple inclusion of rapi_utils.h

--- include/rapi_utils.h
+++ include/rapi_utils.h
@@ -1,5 +1,9 @@
 
+#ifndef __RAPI_UTILS_H__
+#define __RAPI_UTILS_H__
 
 
 #define PDEBUG(...) { fprintf(stderr, "%s(%d) DEBUG: ", __FILE__, __LINE__); fprintf(stderr, __VA_ARGS__); }
 #define PERROR(...) { fprintf(stderr, "%s(%d) ERROR: ", __FILE__, __LINE__); fprintf(stderr, __VA_ARGS__); }
+
+#endif
